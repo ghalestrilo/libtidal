42300c6676426a6815dea560b322bf06ac01d96d
ghalestrilo <ghalestrilo@gmail.com>
2021-05-22 20:55:26 -0300

    ⛏ update setup-autostart.sh

--- scripts/setup-autostart.sh
+++ scripts/setup-autostart.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-if ! sudo grep autostart /var/spool/cron/crontabs/pi;
-  then sudo echo '@reboot cd /home/pi && ./autostart.sh' > /var/spool/cron/crontabs/pi;
+ln -s '~/autostart.sh' 'common/pi/autostart.sh'
+
+if ! sudo ls /var/spool/cron/crontabs/pi; then
+  sudo touch /var/spool/cron/crontabs/pi
+fi
+
+if ! sudo cat /var/spool/cron/crontabs/pi | grep autostart; 
+  then sudo echo '@reboot cd /home/pi && ./autostart.sh' >> /var/spool/cron/crontabs/pi;
 fi
