696e76bbd87e827e978ef2f0ed972e8c21ed455a
ghalestrilo <ghalestrilo@gmail.com>
2021-05-30 14:10:13 +0100

    :sparkles: amidiauto.conf

--- scripts/build-amidiauto.sh
+++ scripts/build-amidiauto.sh
@@ -4,7 +4,7 @@ buildfolder='build/amidiauto'
 [ ! -d 'build' ] && mkdir "build"
 [ ! -d $buildfolder ] && git clone https://github.com/BlokasLabs/amidiauto $buildfolder
 [ ! -d "$buildfolder/build" ] && mkdir "$buildfolder/build"
-cd "$buildfolder/build"
+cd "$buildfolder"
 
 make
 sudo make install
