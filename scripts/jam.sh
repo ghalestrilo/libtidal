255c649dbbfda71e07db909ae9182c18dc851c11
ghalestrilo <ghalestrilo@gmail.com>
2021-06-03 15:27:28 +0100

    :construction: update jam script

---
+++ scripts/jam.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+folder="$1"
+
+# Run a Tidal Jam
+tmux new-session -s jam "zsh -c \"nvim $JAM_PATH\""              \; \
+  new-window sclang                                                      \; \
+  new-window 'cninjam 143.110.158.146:2049 -user anonymous:ghales -jack' \; \
+  new-window 'amidiauto'                                                 \; \
+  select-window -t 0
+
+
