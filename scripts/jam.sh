ad7b1edcc485c0a723dc3cd286676dbf811d000d
ghalestrilo <ghalestrilo@gmail.com>
2021-07-22 22:00:37 -0300

    ⛏ update jam to call fluidsynth

--- scripts/jam.sh
+++ scripts/jam.sh
@@ -6,7 +6,8 @@ folder="$1"
 tmux new-session -s jam "zsh -c \"nvim $JAM_PATH\""                      \; \
   new-window sclang                                                      \; \
   new-window 'cninjam 143.110.158.146:2049 -user anonymous:ghales -jack' \; \
-  new-window                                                             \; \
+  new-window 'fluidsynth "~/Chrono Trigger.sf2"'                         \; \
   select-window -t 0
 
 
+#  new-window 'fluidsynth \"~/Chrono Trigger.sf2\"'                       \; \
